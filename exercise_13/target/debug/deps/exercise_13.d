529b7c27845f9620544e83929471e87186057019
NSergeevich87 <niknikitaser@gmail.com>
2024-03-19 15:48:16 +0100

    microsoft_learn_19/03/2024

---
+++ exercise_13/target/debug/deps/exercise_13.d
@@ -0,0 +1,5 @@
+D:\Rust\exercise_13\target\debug\deps\exercise_13.exe: src\main.rs
+
+D:\Rust\exercise_13\target\debug\deps\exercise_13.d: src\main.rs
+
+src\main.rs:
---
+++ exercise_13/target/debug/exercise_13.d
@@ -0,0 +1,1 @@
+D:\Rust\exercise_13\target\debug\exercise_13.exe: D:\Rust\exercise_13\src\main.rs
